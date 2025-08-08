bc1527ae6c06f4f5c4ccafad3786a3ba0d30f77a
Abhishek-mehta-dev <mehtaabhishek499@gmail.com>
2025-08-08 18:31:57 +0530

    apply the routes for plans and add the plan model in base file for track

--- backend/app/api/routes/__init__.py
+++ backend/app/api/routes/__init__.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
-from . import auth
+from . import auth,plan
 
 api_router = APIRouter()
 api_router.include_router(auth.router)
+api_router.include_router(plan.router)
--- backend/app/db/base.py
+++ backend/app/db/base.py
@@ -2,4 +2,4 @@
 
 from app.db.models.user import User  # Import your models here
 from app.db.models.subscription import Subscription
-
+from app.db.models.plans import Plans
