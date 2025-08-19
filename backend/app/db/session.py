d95c558b243894ae9cc8318aa3a9b84ec9884921
Abhishek-mehta-dev <mehtaabhishek499@gmail.com>
2025-08-19 13:17:36 +0530

    make stable connection with postgres without disconnecting

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -1,12 +1,18 @@
 from sqlmodel import create_engine, Session
 from app.core.config import settings
-
+import app.db.base  
 
 DATABASE_URL = settings.DATABASE_URL
 
-# For async usage: use "postgresql+asyncpg://" and AsyncSession
-engine = create_engine(DATABASE_URL, echo=False)
-
+engine = create_engine(
+    DATABASE_URL,
+    echo=False,
+    pool_pre_ping=True,
+    pool_recycle=300,
+    pool_size=5,
+    max_overflow=10,
+    connect_args={"sslmode": "require"},
+)
 
 def get_session():
     """FastAPI dependency to provide a session per request."""
