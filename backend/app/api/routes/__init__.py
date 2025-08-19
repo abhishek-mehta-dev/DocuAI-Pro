02e5b4ffd24f6995845a066ddba90bf7d682f1a6
Abhishek-mehta-dev <mehtaabhishek499@gmail.com>
2025-08-19 13:18:00 +0530

    rgister the payment routes for usage

--- backend/app/api/routes/__init__.py
+++ backend/app/api/routes/__init__.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
-from . import auth,plan
+from . import auth,plan,payment
 
 api_router = APIRouter()
 api_router.include_router(auth.router)
 api_router.include_router(plan.router)
+api_router.include_router(payment.router)
