e3d0f1bbc3b5f0930f4272276ebcb38bf35aff1a
Abhishek-mehta-dev <mehtaabhishek499@gmail.com>
2025-08-19 13:13:41 +0530

    service and hoooks for payment

---
+++ frontend/docuaipro/src/hooks/usePayment.js
@@ -0,0 +1,33 @@
+import useSWRMutation from "swr/mutation";
+import { createPayment, verifyPayment } from "@/services/paymentService";
+
+export const useCreatePayment = (payload) => {
+  const { data, error, isMutating, trigger } = useSWRMutation(
+    "create-payment",
+    async (_, { arg: payload }) => {
+      return await createPayment(payload);
+    }
+  );
+
+  return {
+    data,
+    isError: Boolean(error),
+    isLoading: isMutating,
+    error,
+    trigger,
+  };
+};
+
+export const useVerifyPayment = () => {
+  const { data, error, isMutating, trigger } = useSWRMutation(
+    "verify-payment",
+    verifyPayment
+  );
+  return {
+    data,
+    isError: Boolean(error),
+    isLoading: isMutating,
+    error,
+    trigger,
+  };
+};
---
+++ frontend/docuaipro/src/services/paymentService.js
@@ -0,0 +1,11 @@
+import apiClient from "@/lib/apiClient";
+
+export const createPayment = async (payload) => {
+  const response = await apiClient.post("/payment/create", payload);
+  return response.data.data;
+};
+
+export const verifyPayment = async (orderId) => {
+  const response = await apiClient.post(`/payment/verify/${orderId}`);
+  return response.data;
+};
